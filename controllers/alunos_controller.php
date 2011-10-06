e60e7c9dbfcd35f3edd31886952a8411f6ebad1d
Thiago Belem <contato@thiagobelem.net>
2011-10-05 22:04:24 -0300

    Controller de alunos

---
+++ controllers/alunos_controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Controller de alunos
+ * 
+ * @author			Thiago Belem <contato@thiagobelem.net>
+ * 
+ * @package 		AssandoSites
+ * @subpackage 	Controller
+ */
+
+/**
+ * Controller de alunos
+ */
+class AlunosController extends AppController {
+	
+}
